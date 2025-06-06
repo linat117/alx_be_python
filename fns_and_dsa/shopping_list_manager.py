62776bf34a4e5b01c618cf6881a37206fbf88bee
Hilina Teshome <teshilina8@gmail.com>
2025-06-06 15:19:42 +0300

    input type updated

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,20 +9,23 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item you want to add to the shopping list: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(shopping_list)
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter the item you want to remove from the shopping list: ")
-            shopping_list.remove(item)
-            print(shopping_list)
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
+               shopping_list.remove(item)
+               print(shopping_list)
+            else:
+                print(f"{item} is not in the list")
         elif choice == '3':
-            print(shopping_list)
+            print(f"shopping list : {shopping_list} ")
         elif choice == '4':
             print("Goodbye!")
             break
